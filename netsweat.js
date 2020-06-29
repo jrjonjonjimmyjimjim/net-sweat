bdcf61d0aae55b2b3e3511224ccd7a1a76148e7b
Jonathon Robel <jonrrobel@gmail.com>
2020-06-28 23:01:28 -0600

    Delay setting works!

--- netsweat.js
+++ netsweat.js
@@ -6,6 +6,8 @@ document.body.style.border = '10px solid red';
 const gettingStoredSettings = browser.storage.local.get();
 gettingStoredSettings.then(checkStoredSettings, onError);
 
+/* BEGIN FUNCTION DECLARATIONS */
+
 function onError(e) {
     console.error(e);
 }
@@ -13,12 +15,18 @@ function onError(e) {
 function checkStoredSettings(storedSettings) {
     var currentTime = new Date();
     var previousTime;
+    var minuteDelay;
+    if (!storedSettings.delay) {
+        minuteDelay = 10;
+    } else {
+        minuteDelay = storedSettings.delay;
+    }
     if (!storedSettings.lastTime) {
         browser.storage.local.set({lastTime});
         alert('You are accessing a Netsuite production account.');
     } else {
         previousTime = Date.parse(storedSettings.lastTime.time);
-        if (currentTime - previousTime > 600000) {
+        if (currentTime - previousTime > 60000 * minuteDelay) {
             alert('You are accessing a Netsuite production account.');
             lastTime.time = new Date();
             browser.storage.local.set({lastTime});
